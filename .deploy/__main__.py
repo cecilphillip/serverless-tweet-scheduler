e58e4c64ceeb7c365fd9811c22fab897d57d1040
cecilphillip <cecilphillip@gmail.com>
2020-03-28 04:43:37 -0400

    add pulumi deployment scripts

---
+++ .deploy/__main__.py
@@ -0,0 +1,53 @@
+import pulumi
+from pulumi_azure import core, storage, servicebus, appservice
+from isodate import Duration, duration_isoformat
+
+# Create an Azure Resource Group
+resource_group = core.ResourceGroup('serverless-scheduler')
+
+# Create an Azure Service Bus namespace
+sbnamespace = servicebus.Namespace('serverless-scheduler-demo',
+                                   sku='Standard',
+                                   name='serverless-scheduler-demo',
+                                   location='EastUs',
+                                   resource_group_name=resource_group.name
+                                   )
+
+# Create an associated queue within the above namespace
+servicebus.Queue('scheduled-tweets',
+                 name='scheduled-tweets',
+                 namespace_name=sbnamespace.name,
+                 dead_lettering_on_message_expiration=True,
+                 resource_group_name=resource_group.name,
+                 max_size_in_megabytes=1024,
+                 default_message_ttl=duration_isoformat(Duration(days=5))
+                 )
+
+# Create an Azure Storage Account
+scheduler_account = storage.Account('schedulerstorage',
+                                    name='schedulerstorage',
+                                    account_kind="StorageV2",
+                                    resource_group_name=resource_group.name,
+                                    account_tier='Standard',
+                                    account_replication_type='LRS')
+
+scheduler_plan = appservice.Plan("serverless-scheduler",
+                                 resource_group_name=resource_group.name,
+                                 kind="FunctionApp",
+                                 sku={
+                                      "tier": "Dynamic",
+                                      "size": "Y1"
+                                 })
+
+appservice.FunctionApp("serverless-scheduler",
+                       resource_group_name=resource_group.name,
+                       app_service_plan_id=scheduler_plan.id,
+                       storage_connection_string=scheduler_account.primary_connection_string,
+                       version="~3",
+                       app_settings={
+                           "runtime": "dotnet"
+                       })
+
+
+# Export the connection string for the storage account
+pulumi.export('connection_string', scheduler_account.primary_connection_string)
