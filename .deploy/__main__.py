e3fd10e4465ce12f026d6615fd15a0c3c253c4b8
cecilphillip <cecilphillip@gmail.com>
2020-03-29 17:42:06 -0400

    include app insights, set os to linux

--- .deploy/__main__.py
+++ .deploy/__main__.py
@@ -1,9 +1,17 @@
 import pulumi
-from pulumi_azure import core, storage, servicebus, appservice
+from pulumi_azure import core, storage, servicebus, appservice, appinsights
 from isodate import Duration, duration_isoformat
 
 # Create an Azure Resource Group
-resource_group = core.ResourceGroup('serverless-scheduler')
+resource_group = core.ResourceGroup('serverless-scheduler',
+                                    name='serverless-scheduler',
+                                    location='EastUs')
+
+app_insights = appinsights.Insights('serverless-scheduler-ai',
+                                    name='serverless-scheduler-ai',
+                                    resource_group_name=resource_group.name,
+                                    location=resource_group.location,
+                                    application_type='web')
 
 # Create an Azure Service Bus namespace
 sbnamespace = servicebus.Namespace('serverless-scheduler-demo',
@@ -24,30 +32,41 @@ servicebus.Queue('scheduled-tweets',
                  )
 
 # Create an Azure Storage Account
-scheduler_account = storage.Account('schedulerstorage',
-                                    name='schedulerstorage',
+scheduler_account = storage.Account('serverlessschedulerstore',
+                                    name='serverlessschedulerstore',
                                     account_kind="StorageV2",
                                     resource_group_name=resource_group.name,
                                     account_tier='Standard',
                                     account_replication_type='LRS')
 
 scheduler_plan = appservice.Plan("serverless-scheduler",
+                                 name='serverless-scheduler',
                                  resource_group_name=resource_group.name,
                                  kind="FunctionApp",
+                                 reserved=True,
                                  sku={
                                       "tier": "Dynamic",
                                       "size": "Y1"
                                  })
 
-appservice.FunctionApp("serverless-scheduler",
+scheduler_app = appservice.FunctionApp("serverless-scheduler",
+                       name="serverless-scheduler",
                        resource_group_name=resource_group.name,
                        app_service_plan_id=scheduler_plan.id,
+                       https_only=True,
+                       os_type="linux",  # doesn't work??
                        storage_connection_string=scheduler_account.primary_connection_string,
                        version="~3",
                        app_settings={
-                           "runtime": "dotnet"
+                           "FUNCTIONS_WORKER_RUNTIME": "dotnet",
+                           "APPINSIGHTS_INSTRUMENTATIONKEY": app_insights.instrumentation_key,
+                           "ServiceBusConnection": sbnamespace.default_primary_connection_string,
+                           "TwitterAPIKey": "",
+                           "TwitterAPISecret": "",
+                           "TwitterAccessToken": "",
+                           "TwitterAccessTokenSecret": ""
                        })
 
-
 # Export the connection string for the storage account
-pulumi.export('connection_string', scheduler_account.primary_connection_string)
+pulumi.export('storage account constr', scheduler_account.primary_connection_string)
+pulumi.export('function app kind', scheduler_app.kind)
